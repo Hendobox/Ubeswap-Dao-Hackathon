c83f3cecbff9e0ee51ee1d8d23510430f5e23a61
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2022-11-27 16:05:17 +0100

    setup frontend repository

---
+++ frontend/src/utils/chakra-themes.ts
@@ -0,0 +1,18 @@
+import { ComponentStyleConfig, extendTheme } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const components: ComponentStyleConfig = {
+  defaultProps: {
+    size: "md",
+  },
+};
+
+export const ChakraTheme = extendTheme({ colors, components });
